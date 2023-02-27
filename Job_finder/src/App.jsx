ac93bd9ccbeeb3265c1ef5d693a8bf0922aa225e
Touisse <yassin.touisd@gmail.com>
2023-02-27 20:24:49 +0100

    nav bar fully responsive

--- Job_finder/src/App.jsx
+++ Job_finder/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Footer from "./Components/FooterDiv/Footer";
 import Jobs from "./Components/JobDiv/Jobs";
-import NavBar from "./Components/NavBar/NavBar";
+import Navbar from "./Components/NavBar/NavBar";
 import Search from "./Components/SearchDiv/Search";
 import Value from "./Components/ValueDiv/Value";
 
 const App = () => {
   return (
     <div className="app">
-      <NavBar />
+      <Navbar />
       <Search/>
       <Jobs/>
       <Value/>
--- Job_finder/src/Components/NavBar/NavBar.jsx
+++ Job_finder/src/Components/NavBar/NavBar.jsx
@@ -1,25 +1,82 @@
-import React from "react";
-import './Navbar.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-const NavBar = () => {
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+  window.addEventListener("resize", showButton);
+
+  useEffect(() => {
+    showButton();
+  }, []);
   return (
-    <div className="navBar">
-      <div className="logoDiv">
-        <h1 className="logo  text-colorBlue ">
-          Job<strong>Search</strong>
-        </h1>
-      </div>
-      <div className="menu">
-        <li className="menuList ">Jobs</li>
-        <li className="menuList ">Companies</li>
-        <li className="menuList ">About</li>
-        <li className="menuList ">Contact</li>
-        <li className="menuList ">Blog</li>
-        <li className="menuList ">Login</li>
-        <li className="menuList ">Register</li>
-      </div>
+    <div>
+      <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <div className="navbar-logo">
+              <strong>Job</strong>Search <i className="fab fa-typo3"></i>
+            </div>
+
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? "fas fa-times" : "fas fa-bars"} />
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <div to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Jobs
+                </div>
+              </li>
+              <li className="nav-item">
+                <div to="/" className="nav-links" onClick={closeMobileMenu}>
+                Contact
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  to="/services"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Companies
+                </div>
+              </li>
+              <li className="nav-item">
+                <div
+                  to="/products"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </div>
+              </li>
+
+              <li className="nav-item">
+                <div
+                  to="/sign-up"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
     </div>
   );
-};
+}
 
-export default NavBar;
+export default Navbar;
